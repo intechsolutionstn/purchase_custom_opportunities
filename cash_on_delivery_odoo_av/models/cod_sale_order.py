e2318454a171349b32e40197a5150423184467be
InTech Solutions <110056927+intechsolutionstn@users.noreply.github.com>
2023-08-26 15:18:02 +0100

    Delete cash_on_delivery_odoo_av directory

--- cash_on_delivery_odoo_av/models/cod_sale_order.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, models, api
-
-class SaleOrder(models.Model):
-    _inherit = "sale.order"
-
-    order_cod_available = fields.Boolean('Allow Cash on Delivery For Sale order', default=False)
