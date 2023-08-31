331eedda728f7159cb37ad2149684abcd580e71d
InTech Solutions <110056927+intechsolutionstn@users.noreply.github.com>
2023-08-31 13:32:41 +0100

    Add files via upload

---
+++ products_shortcut_on_odoo_apps_menu/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#╔══════════════════════════════════════════════════════════════════════╗
+#║                  SOFTWARE DEVELOPED AND SUPPORTED BY                 ║
+#║                  InTech Solutions www.intech-eg.tech                 ║
+#║                      COPYRIGHT (C) 201_ - TODAY                      ║
+#║                      https://www.intech-eg.tech                      ║
+#║                          +216-56-373-373                             ║  
+#║                      support: abnasser@intech-eg.tech                ║
+#╚══════════════════════════════════════════════════════════════════════╝
+
+{
+    'name': 'Products shortcut on odoo apps menu ',
+    'version': '1.2',
+    'category': 'base',
+    'sequence': 35,
+    'description': "This module adds a shortcut to the products menu on the main apps menu in odoo 14",
+    'website': 'https://www.intech-eg.tech',
+    'depends': ['product','sale', 'board'],
+    'author': 'InTech Solutions -- Rabeb Mahmoudi',
+    'support': 'odooappssupport@intech-eg.tech',
+    
+    'data': [
+        'views/menu.xml',
+    ],
+    'qweb': [
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'license': 'LGPL-3',
+}
